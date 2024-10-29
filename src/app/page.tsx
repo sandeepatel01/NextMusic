e17ca670292aeb49985bd7abcbd626e73b137131
sandeepatel01 <sandeepatel.0101@gmail.com>
2024-10-29 11:38:40 +0530

    Add HeroSection-> Spotlight component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-// import { div } from "framer-motion/client";
-// import Image from "next/image";
-
-import { main } from "framer-motion/client";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="max-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">NEXT MUSIC</h1>
+      <HeroSection />
     </main>
   );
 }
